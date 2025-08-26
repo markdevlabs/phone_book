f8c95a0a1d014e3a4bb8122876b57d6c085b6ef8
Mark Kovalev <mark.kovalev7771@gmail.com>
2025-08-26 13:19:26 +0300

    I created code generator

--- tests.py
+++ tests.py
@@ -1,8 +1,19 @@
-class Phone:
-    def __init__(self, brand: str, price: float):
-        self.brand = brand
-        self.price = price
+import random
+import string
 
-new_phone = Phone("Samsung", 49500.55)
-print(new_phone.brand)
-print(new_phone.price)
+
+
+def create_and_add_values():
+    code_list = []
+    all_letters = string.ascii_letters
+    for i in range(0, 5):
+        generate_num = random.randint(0, 10)
+        generate_letter = random.choice(all_letters)
+        code_list.append(generate_num)
+        code_list.append(generate_letter)
+
+    print(code_list)
+
+
+create_and_add_values()
+
